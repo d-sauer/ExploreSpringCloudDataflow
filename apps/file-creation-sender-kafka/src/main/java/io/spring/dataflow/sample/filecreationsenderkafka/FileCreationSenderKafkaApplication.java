c1bce08ca07e68c20947c6d901afa8bc18a3c5c9
d-sauer <davor.sauer@gmail.com>
2019-07-29 17:24:53 +0200

    Create sender application for file creation

---
+++ apps/file-creation-sender-kafka/src/main/java/io/spring/dataflow/sample/filecreationsenderkafka/FileCreationSenderKafkaApplication.java
@@ -0,0 +1,13 @@
+package io.spring.dataflow.sample.filecreationsenderkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileCreationSenderKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileCreationSenderKafkaApplication.class, args);
+	}
+
+}
